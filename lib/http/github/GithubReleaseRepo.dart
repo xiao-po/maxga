17fe1f000f139228e9b264e0aa512814f6b43cfb
wangluyao <wly19960911@gmail.com>
2019-11-21 21:00:41 +0800

    修改更新问题

--- lib/http/github/GithubReleaseRepo.dart
+++ lib/http/github/GithubReleaseRepo.dart
@@ -5,7 +5,7 @@ import 'package:http/http.dart' as http;
 import 'package:maxga/model/github/GithubRelease.dart';
 class GithubReleaseRepo {
   static Future<GithubRelease> getLatestReleaseInfo() async  {
-    final response = await http.get('https://api.github.com/repos/xiao-po/maxga/releases/21522995');
+    final response = await http.get('https://api.github.com/repos/xiao-po/maxga/releases/latest');
     return GithubRelease.fromJson(json.decode(response.body));
   }
 }
